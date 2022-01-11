43ad4672a96a8eb6a7f0679981f518fcbdb6d46c
ashishyete <ashish.yete@gmail.com>
2022-01-11 18:08:21 +0530

    Change in Cors Configuration

--- src/main/java/com/example/configuration/CorsConfiguration.java
+++ src/main/java/com/example/configuration/CorsConfiguration.java
@@ -1,11 +1,12 @@
 package com.example.configuration;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
-public class CorsConfiguration implements WebMvcConfigurer {
+public class CorsConfiguration{
 
     private static final String GET = "GET";
     private static final String PUT = "PUT";
@@ -13,12 +14,17 @@ public class CorsConfiguration implements WebMvcConfigurer {
     private static final String DELETE = "DELETE";
 
 
-    @Override
-    public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**")
-                .allowedMethods(GET,POST,PUT,DELETE)
-                .allowedHeaders("*")
-                .allowedOriginPatterns("*")
-                .allowCredentials(true);
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**")
+                        .allowedMethods(GET, POST, PUT, DELETE)
+                        .allowedHeaders("*")
+                        .allowedOriginPatterns("*")
+                        .allowCredentials(true);
+            }
+        };
     }
 }
